b9dfb620d269845be4e530ed3357f434440f243c
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 21:05:54 +0300

    add a Neighbourhood class that will set how the Neighbourhood will look like

--- app/models.py
+++ app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class NeighbourHood(models.Model):
+    admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
+    name = models.CharField(max_length=50)
+    location = models.CharField(max_length=60)
+    occupants=models.IntegerField(null=True, blank=True)
+    description = models.TextField()
+    
