d0a6504fd48206444282d7092156c8c8274dd8de
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 21:47:18 +0300

    add vindex view funtion to display index.html page

--- app/views.py
+++ app/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from app.models import *
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def index(request):
+    return render (request, 'index.html')
