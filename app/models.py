5adaf98e07e5e29a9caeed83283b5fe266c76977
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 21:28:45 +0300

    add a asearch functionality to search the business using its id

--- app/models.py
+++ app/models.py
@@ -54,4 +54,8 @@ class Business(models.Model):
 
     def delete_business(self):
         self.delete()
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(name__icontains=name).all()
     
