f9fcc769a0cb99202c57c605d46cca2abe814579
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 21:20:45 +0300

    add a save method method thst will save every instance of the profile

--- app/models.py
+++ app/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class NeighbourHood(models.Model):
     admin = models.ForeignKey("Profile", on_delete=models.CASCADE, related_name='hood')
     name = models.CharField(max_length=50)
     location = models.CharField(max_length=60)
-    occupants=models.IntegerField(null=True, blank=True)
+    occupants_count=models.IntegerField(null=True, blank=True)
     description = models.TextField()
     
     def __str__(self):
@@ -17,5 +18,19 @@ class NeighbourHood(models.Model):
         self.delete()
 
     @classmethod
-    def get_neighborhood(cls, N_hood_id):
+    def search_neighborhood(cls, N_hood_id):
         return cls.objects.filter(id=N_hood_id)
+
+class Profile(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    name = models.CharField(max_length=80, blank=True)
+    profile_picture = models.ImageField(upload_to='images/', default='default.png')
+    location = models.CharField(max_length=50, blank=True, null=True)
+    neighbourhood = models.ForeignKey(NeighbourHood, on_delete=models.SET_NULL, null=True, related_name='hood_member', blank=True)
+
+    def __str__(self):
+        return f'{self.user.username} profile'
+
+    def save_profile(self):
+        self.save()
