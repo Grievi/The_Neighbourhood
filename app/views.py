a19c70390a57a8d1e0441bd08562878151d11962
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-01 22:41:10 +0300

    add a login function to the project

--- app/views.py
+++ app/views.py
@@ -1,7 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from app.models import *
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages 
+from django.contrib.auth import login,authenticate,logout
 
 
 def index(request):
     return render (request, 'index.html')
+
+def user_login(request):
+    message = 'Login to Proceed'
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request,f"Welcome {username} to Movie Gallore!")
+            return redirect('home')
+
+        else:
+            messages.success(request,"Oops Somethinge went wrong, please Login!")
+
+            return render(request, 'registration/login.html')
+    else:
+        return render(request, 'registration/login.html', {"message": message})
