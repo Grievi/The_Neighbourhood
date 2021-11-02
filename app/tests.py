e1ae68304ebded8b680fd6fa84b0411218447fca
grievin.okumu <grievin.okumu@student.moringaschool.com>
2021-11-02 20:49:00 +0300

    add one test to test class NeighbourHood

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from app.models import *
 
-# Create your tests here.
+class NeighbourhoodTestClass(TestCase):
+    def setUp(self):
+        # create an admin user
+        self.admin = User.objects.create_superuser(
+            username='admin',
+            password='admin123'
+        )
+        self.neighbourhood = NeighbourHood(
+            name='This is a test', location=self.location, occupants_count=100, admin_id=self.admin.id)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.neighbourhood, NeighbourHood))
+
+    def test_save_method(self):
+        self.neighbourhood.create_neigbourhood()
+        neighbourhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighbourhoods) > 0)
+
+    def test_delete_method(self):
+        self.neighbourhood.create_neigborhood()
+        self.neighbourhood.delete()
+        neighbourhoods = NeighbourHood.objects.all()
+        self.assertTrue(len(neighbourhoods) == 0)
